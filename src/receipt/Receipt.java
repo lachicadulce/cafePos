be271e51b6dee88b32362c0470bb7743cc0a8459
Yoo <Yoo@192.168.0.104>
2021-08-04 23:14:49 +0900

    시간까지 추가함

--- src/receipt/Receipt.java
+++ src/receipt/Receipt.java
@@ -1,9 +1,12 @@
 package receipt;
 
 import java.awt.BorderLayout;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 import javax.swing.JButton;
+import javax.swing.JLabel;
 
 import baseSettings.PosFrame;
 
@@ -14,20 +17,27 @@ public class Receipt extends PosFrame {
 		
 		setLayout(null);
 		
+		// 시간을 문자열로 변경하기
+		DateTimeFormatter my_date_format = 
+				DateTimeFormatter.ofPattern("y년 M월 d일");
+		
+		String message = my_date_format.format(LocalDate.now());
+		
+		
 		ArrayList<JButton> buttons = new ArrayList<>();
-		buttons.add(new JButton("전표반품"));
+		JButton date = new JButton(message);
+		
+		buttons.add(new JButton("전표반품")); // 0 
 		buttons.add(new JButton("재인쇄"));
 		buttons.add(new JButton("현금영수증"));
+		
 		buttons.add(new JButton("결제변경"));
 		buttons.add(new JButton("전체"));
 		buttons.add(new JButton("현금"));
-		buttons.add(new JButton("신용카드"));
-//		frame.add(new JButton("Button 3") {
-//			{
-//				setLocation(30, 30);
-//				setSize(150, 80);
-//			}
-//		});
+		buttons.add(new JButton("신용카드")); // 6
+		
+		
+		// 버튼 위치 조정
 		buttons.get(0).setBounds(20, 30, 150, 80);
 		buttons.get(1).setBounds(190, 30, 150, 80);
 		buttons.get(2).setBounds(360, 30, 150, 80);
@@ -37,14 +47,23 @@ public class Receipt extends PosFrame {
 		buttons.get(5).setBounds(1010, 30, 120, 80);
 		buttons.get(6).setBounds(1150, 30, 120, 80);
 		
+		date.setBounds(730, 30, 150, 80);
+		
+		// 버튼 추가
 		add(buttons.get(0));
 		add(buttons.get(1));
 		add(buttons.get(2));
 		add(buttons.get(3));
 		
+		add(date);
+		
 		add(buttons.get(4));
 		add(buttons.get(5));
 		add(buttons.get(6));
+		
+		
+		
+		
 		
 		
 		

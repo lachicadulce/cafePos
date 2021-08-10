3de664e0a28e01ed4182dae6d7acb1457a8116e7
SeungMin369 <usm369@naver.com>
2021-08-10 16:26:26 +0900

    [유승민][receipt] 영수증 DB 연계 자동완성 완료 Receipt
    좌측 목록 클릭시에 우측 영수증이 바뀌게 만들기 위해서
    데이터베이스 커넥션과 영수증 출력 시스템을 void화 했습니다.

--- src/receipt/Receipt.java
+++ src/receipt/Receipt.java
@@ -34,105 +34,32 @@ public class Receipt extends PosFrame {
 	
 	
 	static ArrayList<ArrayList<String>> list_data = new ArrayList<ArrayList<String>>();
-	String[] columnNames = null;
-	String RECEIPT_NO = "24";
-	HashMap<String, ArrayList<Integer>> drink;
+	static String[] columnNames = null;
+	static String RECEIPT_NO = "24";
+	static HashMap<String, ArrayList<Integer>> drink;
 	
-	int total;
-	int vat;
-	int cash;
-	int point_used;
-	int point;
-	int credit;
-	String receipt_chk;
-	Date transaction_date;
+	static String string;
 	
-	DecimalFormat formatter = new DecimalFormat("##,###,###"); // 금액 출력 포멧터
+	static int total;
+	static int vat;
+	static int cash;
+	static int point_used;
+	static int point;
+	static int credit;
+	static String receipt_chk;
+	static Date transaction_date;
+	
+	static DecimalFormat formatter = new DecimalFormat("##,###,###"); // 금액 출력 포멧터
 	
 	
 	public Receipt() {
 		super();
 		super.setTitle("영수증 관리");
-		try {
-            Connection conn = DriverManager.getConnection(
-            		"jdbc:oracle:thin:@database-1.cxc98ia1oha4.us-east-2.rds.amazonaws.com:1521/ORCL",
-            		"cafe",
-            		"!!22Qorthdud");
+		connection(); // void 함수로 뺐어요 ~
 
-            String sql = "select * from PAYMENT_VIEW WHERE RECEIPT_NO = " + RECEIPT_NO;
-            
-            // ================================================================================================
-            // ================================================================================================            	
-            	String Receipt_list = "select * from payment_view_1 ORDER BY DATETIME";
-            	PreparedStatement pstmt_Receipt_list = conn.prepareStatement(Receipt_list);
-            	
-            	ResultSet rs_list = pstmt_Receipt_list.executeQuery();
-            	
-            	ResultSetMetaData md = rs_list.getMetaData() ;
-            	int column_size = md.getColumnCount();
-            	
-            	columnNames = new String[column_size+1];
-            	
-            	System.out.println(columnNames.length);
-            	for(int i = 0; i < columnNames.length; i++) {
-            		System.out.print(i + " ");
-            		if (i == 0) {
-            			System.out.println(i);
-            			columnNames[i] = "No";
-            		} else {
-            			System.out.println(md.getColumnName(i));
-            			columnNames[i] = md.getColumnName(i);
-            		}
-                }
-
-            	while (rs_list.next()) {
-            		ArrayList<String> data = new ArrayList<>();
-            		for (int i = 0; i < columnNames.length; i++) {
-            			if (i == 0) {
-            				data.add("" + i);
-            			} else {
-            				data.add(rs_list.getString(columnNames[i]));
-            			}
-            		}
-            		list_data.add(data);
-            		
-            	}
-            	rs_list.close();
-            	pstmt_Receipt_list.close();
-            
-            // ================================================================================================
-        	// ================================================================================================
-            PreparedStatement pstmt = conn.prepareStatement(sql);
-
-            ResultSet rs = pstmt.executeQuery();
-            drink = new HashMap<>();
-            while (rs.next()) {
-            	// HashMap에 음료수 내역 넣기
-            	drink.putIfAbsent(rs.getString("MENU"), new ArrayList<>());
-            	
-            	drink.get(rs.getString("MENU")).add(rs.getInt("PRICE"));
-            	
-            	total = rs.getInt("TOTAL");
-            	vat = rs.getInt("TOTAL") / 10;
-            	cash = rs.getInt("CASH");
-            	point_used = rs.getInt("POINT_USED");
-            	point = rs.getInt("POINT");
-            	credit = rs.getInt("CREDIT");
-            	receipt_chk = rs.getString("RECEIPT_CHK");
-            	transaction_date = rs.getDate("DATETIME");
-            }
-
-            rs.close();
-            pstmt.close();
-            conn.close();
-
-        } catch (SQLException e) {
-            System.out.println("getConnection 하다가 문제 생김");
-        }
-		
 		setLayout(null);
 		
-		
+		receipt_print();
 		
 		// 시간을 문자열로 변경하기
 		DateTimeFormatter my_date_format = 
@@ -143,8 +70,162 @@ public class Receipt extends PosFrame {
 		JButton date = new JButton(message);
 		
 		
-		String product = "";
 		
+		
+		
+		
+		
+		
+		
+		
+		
+		JPanel frame = new JPanel();
+		
+		JLabel receipt = new JLabel(string);
+		
+		
+		
+		ArrayList<JButton> buttons = new ArrayList<>();
+		
+		
+		
+		buttons.add(new JButton("전표반품")); // 0 
+		buttons.add(new JButton("재인쇄"));
+		buttons.add(new JButton("현금영수증"));
+		
+		buttons.add(new JButton("결제변경"));
+		buttons.add(new JButton("전체"));
+		buttons.add(new JButton("현금"));
+		buttons.add(new JButton("신용카드")); // 6
+		
+		// 버튼 위치 조정
+		buttons.get(0).setBounds(20, 30, 150, 80);
+		buttons.get(1).setBounds(190, 30, 150, 80);
+		buttons.get(2).setBounds(360, 30, 150, 80);
+		buttons.get(3).setBounds(530, 30, 150, 80);
+		
+		buttons.get(4).setBounds(870, 30, 120, 80);
+		buttons.get(5).setBounds(1010, 30, 120, 80);
+		buttons.get(6).setBounds(1150, 30, 120, 80);
+		
+		for (int i = 0; i < buttons.size(); ++i) {
+			buttons.get(i).setFont(new Font("MV Bold", Font.BOLD, 20));
+		}
+		
+		date.setBounds(730, 30, 120, 80);
+		date.setBackground(new Color(0xffffff));
+		
+		receipt.setFont(new Font("MV Bold", Font.BOLD, 20));
+		receipt.setOpaque(true); // 백그라운드 색상 허용
+		receipt.setHorizontalAlignment(JLabel.CENTER); // 수평 가운데 표시 설정
+		receipt.setVerticalAlignment(JLabel.NORTH);; // 세로 가운데 표시 설정
+		receipt.setBackground(new Color(0xffffff));// 라벨 배경색
+		receipt.setBounds(730, 140, 450, 500);
+//		frame.setBackground(new Color(0xffffff)); // 패널 배경색
+		frame.setBounds(730, 140, 540, 500);
+		receipt.setAutoscrolls(true);
+		JScrollPane scrollPane = new JScrollPane( // 영수증에 수직 스크롤바 추가
+				receipt, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		scrollPane.setPreferredSize(new Dimension(540, 500)); // 스크롤바 크기조정
+//		scrollPane.setBackground(new Color(0xffffff)); 
+		
+		frame.add(scrollPane);
+		
+		// 좌측 위 버튼 추가
+		add(buttons.get(0));
+		add(buttons.get(1));
+		add(buttons.get(2));
+		add(buttons.get(3));
+		
+		add(date); // 날짜 출력
+		
+		// 우측 위 버튼
+		add(buttons.get(4));
+		add(buttons.get(5));
+		add(buttons.get(6));
+		
+        add(frame); 
+        
+     // ===============================================================================================
+     // ================================================================================================
+
+        JPanel receipt_panel = new JPanel();        
+        
+     	receipt_panel.setBackground(Color.black);
+     	receipt_panel.setLocation(20, 140);
+     	receipt_panel.setSize(660, 500);
+
+     	String[][] data1 = null;
+     	
+     	int w_size = list_data.size(), h_size = list_data.get(0).size();
+     	
+     	data1 = new String[w_size+1][h_size];
+     	
+     	for (int i = 0; i < w_size; i++) {
+    		for (int x = 0; x < h_size; x++) {
+    			if (x == 0) {
+    				data1[i][x] = "" + i;
+    			}else {
+	    			data1[i][x] = list_data.get(i).get(x);
+    			}
+    		}
+    	}
+     	
+     	JPanel a = new JPanel();
+     	
+     	JTable table = new JTable(data1, columnNames);
+     	JScrollPane scrollPane1 = new JScrollPane(table);
+     	
+     	scrollPane1.setBackground(Color.pink);
+     	
+     	table.setPreferredSize(new Dimension(660, 1000));
+     	scrollPane1.setPreferredSize(new Dimension(660, 1000));
+//     	승민 영수증넘버 받아오는 로직-----------------------------------------------------------
+     	ListSelectionModel selection = table.getSelectionModel();
+		
+		selection.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		table.setRowSelectionAllowed(false);
+		
+		
+		selection.addListSelectionListener(new ListSelectionListener() {
+		
+			public void valueChanged(ListSelectionEvent e) {
+				// 클릭한곳의 RECEIPT_NO 반환하는 로직
+				RECEIPT_NO = String.valueOf(table.getValueAt(table.getSelectedRow(), 2));
+				
+				connection();
+				
+				receipt_print();
+				
+				receipt.setText(string); // 영수증 바뀌게 하는 처리 해야한다.
+				
+				add(frame);
+				
+				
+				
+			} 
+		
+				
+		});	
+//     	-------------------------------------------------------------------------------------------------
+     	table.getTableHeader().setPreferredSize(new Dimension(scrollPane1.getWidth(), 50));
+     	
+     	table.setRowSelectionAllowed(true);
+     	table.setColumnSelectionAllowed(true);
+     	
+     	table.setShowGrid(true);
+
+     	a.add(scrollPane1);
+     	receipt_panel.add(scrollPane1);
+     	add(receipt_panel);
+     // ================================================================================================
+     // ================================================================================================
+        
+        
+
+	}
+	private static void receipt_print() {
+		String product = "";
 		// 받아온 HashMap 음료 수량 가격 영수증에 출력
 		for (Entry<String, ArrayList<Integer>> entry : drink.entrySet()) {
 			String print = "";
@@ -233,7 +314,7 @@ public class Receipt extends PosFrame {
 		String transaction_date_string = my_date_format2.format(localDateTime);	// 판매일자
 		String returnDayString = my_date_format3.format(returnDay); // 반품가능일자
 		
-		String string = // 영수증 전체 내용 
+		string = // 영수증 전체 내용 
 				"<html><center><br>"
 				+ "매&emsp출&emsp전&emsp표"
 				+ "<br>"
@@ -294,149 +375,89 @@ public class Receipt extends PosFrame {
 				+ "<center>감사합니다.<br><br> </center>"
 				+ "</html>";
 		
-		
-		
-		
-		
-		// &nbsp 띄어쓰기 &emsp 크게 띄어쓰기
-		// <html> +  + <br> +  + </html> 줄 바꾸는 법
-		
-		JPanel frame = new JPanel();
-		
-		JLabel receipt = new JLabel(string);
-		
-		
-		
-		ArrayList<JButton> buttons = new ArrayList<>();
-		
-		
-		
-		buttons.add(new JButton("전표반품")); // 0 
-		buttons.add(new JButton("재인쇄"));
-		buttons.add(new JButton("현금영수증"));
-		
-		buttons.add(new JButton("결제변경"));
-		buttons.add(new JButton("전체"));
-		buttons.add(new JButton("현금"));
-		buttons.add(new JButton("신용카드")); // 6
-		
-		// 버튼 위치 조정
-		buttons.get(0).setBounds(20, 30, 150, 80);
-		buttons.get(1).setBounds(190, 30, 150, 80);
-		buttons.get(2).setBounds(360, 30, 150, 80);
-		buttons.get(3).setBounds(530, 30, 150, 80);
-		
-		buttons.get(4).setBounds(870, 30, 120, 80);
-		buttons.get(5).setBounds(1010, 30, 120, 80);
-		buttons.get(6).setBounds(1150, 30, 120, 80);
-		
-		for (int i = 0; i < buttons.size(); ++i) {
-			buttons.get(i).setFont(new Font("MV Bold", Font.BOLD, 20));
-		}
-		
-		date.setBounds(730, 30, 120, 80);
-		date.setBackground(new Color(0xffffff));
-		
-		receipt.setFont(new Font("MV Bold", Font.BOLD, 20));
-		receipt.setOpaque(true); // 백그라운드 색상 허용
-		receipt.setHorizontalAlignment(JLabel.CENTER); // 수평 가운데 표시 설정
-		receipt.setVerticalAlignment(JLabel.NORTH);; // 세로 가운데 표시 설정
-		receipt.setBackground(new Color(0xffffff));// 라벨 배경색
-//		receipt.setBounds(730, 140, 540, 2000);
-//		frame.setBackground(new Color(0xffffff)); // 패널 배경색
-		frame.setBounds(730, 140, 540, 500);
-		receipt.setAutoscrolls(true);
-		JScrollPane scrollPane = new JScrollPane( // 영수증에 수직 스크롤바 추가
-				receipt, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-		scrollPane.setPreferredSize(new Dimension(540, 500)); // 스크롤바 크기조정
-		
-		frame.add(scrollPane);
-		
-		// 좌측 위 버튼 추가
-		add(buttons.get(0));
-		add(buttons.get(1));
-		add(buttons.get(2));
-		add(buttons.get(3));
-		
-		add(date); // 날짜 출력
-		
-		// 우측 위 버튼
-		add(buttons.get(4));
-		add(buttons.get(5));
-		add(buttons.get(6));
-		
-        add(frame); 
-        
-     // ===============================================================================================
-     // ================================================================================================
-
-        JPanel receipt_panel = new JPanel();        
-        
-     	receipt_panel.setBackground(Color.black);
-     	receipt_panel.setLocation(20, 140);
-     	receipt_panel.setSize(660, 500);
-
-     	String[][] data1 = null;
-     	
-     	int w_size = list_data.size(), h_size = list_data.get(0).size();
-     	
-     	data1 = new String[w_size+1][h_size];
-     	
-     	for (int i = 0; i < w_size; i++) {
-    		for (int x = 0; x < h_size; x++) {
-    			if (x == 0) {
-    				data1[i][x] = "" + i;
-    			}else {
-	    			data1[i][x] = list_data.get(i).get(x);
-    			}
-    		}
-    	}
-     	
-     	JPanel a = new JPanel();
-     	
-     	JTable table = new JTable(data1, columnNames);
-     	JScrollPane scrollPane1 = new JScrollPane(table);
-     	
-     	scrollPane1.setBackground(Color.pink);
-     	
-     	table.setPreferredSize(new Dimension(660, 1000));
-     	scrollPane1.setPreferredSize(new Dimension(660, 1000));
-//     	승민 영수증넘버 받아오는 로직-----------------------------------------------------------
-     	ListSelectionModel selection = table.getSelectionModel();
-		
-		selection.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		table.setRowSelectionAllowed(false);
-		
-		
-		selection.addListSelectionListener(new ListSelectionListener() {
-		
-			public void valueChanged(ListSelectionEvent e) {
-				RECEIPT_NO = String.valueOf(table.getValueAt(table.getSelectedRow(), 2));// 클릭한곳의 RECEIPT_NO 반환하는 로직
-				
-				
-				receipt.setText(string); // 영수증 바뀌게 하는 처리 해야한다.
-				
-			} 
-		
-				
-		});	
-//     	-------------------------------------------------------------------------------------------------
-     	table.getTableHeader().setPreferredSize(new Dimension(scrollPane1.getWidth(), 50));
-     	
-     	table.setRowSelectionAllowed(true);
-     	table.setColumnSelectionAllowed(true);
-     	
-     	table.setShowGrid(true);
-
-     	a.add(scrollPane1);
-     	receipt_panel.add(scrollPane1);
-     	add(receipt_panel);
-     // ================================================================================================
-     // ================================================================================================
-        
-        
-
+				// &nbsp 띄어쓰기 &emsp 크게 띄어쓰기
+				// <html> +  + <br> +  + </html> 줄 바꾸는 법
 	}
+	
+	private static void connection() {
+		try {
+            Connection conn = DriverManager.getConnection(
+            		"jdbc:oracle:thin:@database-1.cxc98ia1oha4.us-east-2.rds.amazonaws.com:1521/ORCL",
+            		"cafe",
+            		"!!22Qorthdud");
+
+            String sql = "select * from PAYMENT_VIEW WHERE RECEIPT_NO = " + RECEIPT_NO;
+            
+            // ================================================================================================
+            // ================================================================================================            	
+            	String Receipt_list = "select * from payment_view_1 ORDER BY DATETIME";
+            	PreparedStatement pstmt_Receipt_list = conn.prepareStatement(Receipt_list);
+            	
+            	ResultSet rs_list = pstmt_Receipt_list.executeQuery();
+            	
+            	ResultSetMetaData md = rs_list.getMetaData() ;
+            	int column_size = md.getColumnCount();
+            	
+            	columnNames = new String[column_size+1];
+            	
+            	System.out.println(columnNames.length);
+            	for(int i = 0; i < columnNames.length; i++) {
+            		System.out.print(i + " ");
+            		if (i == 0) {
+            			System.out.println(i);
+            			columnNames[i] = "No";
+            		} else {
+            			System.out.println(md.getColumnName(i));
+            			columnNames[i] = md.getColumnName(i);
+            		}
+                }
+
+            	while (rs_list.next()) {
+            		ArrayList<String> data = new ArrayList<>();
+            		for (int i = 0; i < columnNames.length; i++) {
+            			if (i == 0) {
+            				data.add("" + i);
+            			} else {
+            				data.add(rs_list.getString(columnNames[i]));
+            			}
+            		}
+            		list_data.add(data);
+            		
+            	}
+            	rs_list.close();
+            	pstmt_Receipt_list.close();
+            
+            // ================================================================================================
+        	// ================================================================================================
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+
+            ResultSet rs = pstmt.executeQuery();
+            drink = new HashMap<>();
+            while (rs.next()) {
+            	// HashMap에 음료수 내역 넣기
+            	drink.putIfAbsent(rs.getString("MENU"), new ArrayList<>());
+            	
+            	drink.get(rs.getString("MENU")).add(rs.getInt("PRICE"));
+            	
+            	total = rs.getInt("TOTAL");
+            	vat = rs.getInt("TOTAL") / 10;
+            	cash = rs.getInt("CASH");
+            	point_used = rs.getInt("POINT_USED");
+            	point = rs.getInt("POINT");
+            	credit = rs.getInt("CREDIT");
+            	receipt_chk = rs.getString("RECEIPT_CHK");
+            	transaction_date = rs.getDate("DATETIME");
+            }
+
+            rs.close();
+            pstmt.close();
+            conn.close();
+
+        } catch (SQLException e) {
+            System.out.println("getConnection 하다가 문제 생김");
+        }
+	}
+	
 	
 	public static void main(String[] args) {
 		 Receipt frame = new Receipt();	

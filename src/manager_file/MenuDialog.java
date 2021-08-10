41baa637ff866c5caa9bfd2103d9f9604778d9dc
lachicadulce <sybaek94@daum.net>
2021-08-10 23:09:45 +0900

    [bsy][manager_file]Add add type function in MenuDialog.java

--- src/manager_file/MenuDialog.java
+++ src/manager_file/MenuDialog.java
@@ -26,25 +26,28 @@ public class MenuDialog extends JDialog {
 	 	# 메뉴 추가 팝업 창
 	 		- 메뉴 이름, 가격, 분류 입력 후 추가 버튼을 누르면 menu 테이블에 추가.
 	 		- display_order는 분류의 마지막 번호 + 1로 추가.
+	 		- 분류가 추가되면 콤보박스에도 새로 추가, display_order는 1로 추가.
 	 */
 	
 	JTextField tf_name = new JTextField(20);
 	JTextField tf_price = new JTextField(20);
+	JTextField tf_type = new JTextField(10);
 	
-	ArrayList<String> types = new ArrayList();
-	String types1[] = {"coffee", "tea", "MD"};
+	ArrayList<String> types;
 	
 	JComboBox<String> cb_type;
 	JButton btn_add = new JButton("추가");
 	JButton btn_can = new JButton("취소");
 	
+	
 	public MenuDialog(JFrame frame, String title) {		
 		super(frame, title);
 
+		types = new ArrayList();
 		selTypes();
 		cb_type = new JComboBox<String>(types.toArray(new String[types.size()]));
-		cb_type.setPreferredSize(new Dimension(210, 20));
-		btn_add.setSize(new Dimension(150, 20));
+		cb_type.setPreferredSize(new Dimension(100, 20));
+//		btn_add.setSize(new Dimension(150, 20));
 		
 		setLayout(new FlowLayout());
 		add(new JLabel("이름"));
@@ -53,9 +56,11 @@ public class MenuDialog extends JDialog {
 		add(tf_price);
 		add(new JLabel("분류"));
 		add(cb_type);
+		add(tf_type);
 		add(btn_add);
 		add(btn_can);
-		setSize(270, 200);
+		setSize(270, 160);
+		setLocation(100, 100);
 		
 		btn_add.addActionListener(new ActionListener() {
 			
@@ -66,13 +71,35 @@ public class MenuDialog extends JDialog {
 					System.out.println("입력하신 정보가 올바르지 않습니다. 다시 확인해 주시기 바랍니다.");
 					return;
 				}
-				
-				// 콤보 박스의 인덱스에 맞춰서 분류 이름 가져오기.
-				String type_name = types.get(cb_type.getSelectedIndex());
+
 				InsertDB insert = new InsertDB();
-				insert.dbinsert("INSERT INTO menu VALUES((SELECT MAX(menu_no) + 1 FROM menu), '" + tf_name.getText() + "', " + tf_price.getText() + ", '" + type_name + "', (SELECT MAX(display_order) + 1 FROM menu WHERE type = '" + type_name + "'), 'Y')");
+				// 콤보 박스의 인덱스에 맞춰서 분류 이름 가져오기.
+				String type_name;
+				// 분류를 추가한 경우
+				if(cb_type.getSelectedIndex() == cb_type.getItemCount() - 1) {
+					type_name = tf_type.getText();
+					if(type_name.equals("")) {
+						System.out.println("입력하신 정보가 올바르지 않습니다. 다시 확인해 주시기 바랍니다.");
+						return;
+					}
+					insert.dbinsert("INSERT INTO menu VALUES((SELECT MAX(menu_no) + 1 FROM menu), '" + tf_name.getText() + "', " + tf_price.getText() + ", '" + type_name + "', 1, 'Y')");
+					
+					// 분류 새로 가져오기.
+					types.clear();
+					selTypes();
+					cb_type.removeAllItems();
+					for(String type : types) {
+						cb_type.addItem(type);
+					}
+				} else { // 분류 추가 없이 기존 분류에 추가한 경우
+					type_name = types.get(cb_type.getSelectedIndex());
+					insert.dbinsert("INSERT INTO menu VALUES((SELECT MAX(menu_no) + 1 FROM menu), '" + tf_name.getText() + "', " + tf_price.getText() + ", '" + type_name + "', (SELECT MAX(display_order) + 1 FROM menu WHERE type = '" + type_name + "'), 'Y')");
+				}
+				
+				// 초기화
 				tf_name.setText("");
 				tf_price.setText("");
+				tf_type.setText("");
 				cb_type.setSelectedIndex(0);
 			}
 		});
@@ -88,6 +115,8 @@ public class MenuDialog extends JDialog {
 			}
 		});
 	}
+	
+	// 분류 db에서 가져오기.
 	private void selTypes() {
 		String sql = "SELECT DISTINCT type FROM menu";
 	    try (
@@ -102,6 +131,7 @@ public class MenuDialog extends JDialog {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+	    types.add("추가 (기입)");
 	}
 	
 }

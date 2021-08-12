f0a6a5dfdcaac79e43d7d97bf674864ad73c95dd
tnghk <tnghk@DESKTOP-5QHHHV2>
2021-08-12 20:54:08 +0900

    [KSH] merge함

---
+++ src/handler/ManagerButtonActionListener.java
@@ -0,0 +1,27 @@
+package handler;
+
+import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import manager_file.ManagerSales;
+
+public class ManagerButtonActionListener implements ActionListener {
+
+	Frame frame;
+	public ManagerButtonActionListener(Frame frame) {
+		this.frame = frame;
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		frame.dispose();
+		frame.validate();
+		frame.invalidate();
+		
+		ManagerSales ms = new ManagerSales();
+		ms.setDefaultOptions();
+
+	}
+
+}
